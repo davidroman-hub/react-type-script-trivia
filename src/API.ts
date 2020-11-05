a320c81372b546f8699c17bf27f04e4ab7f986ab
David <jobroman83@gmail.com>
2020-11-05 14:36:38 +0100

    spriding the correct answer and the incorrect answer in console

--- src/API.ts
+++ src/API.ts
@@ -1,4 +1,20 @@
 
+import {shuffleArray} from './utils';
+
+
+// Remember this info below comes from the response in console log
+
+export type Question = {
+    category:string;
+    correct_answer:string;
+    difficulty:string;
+    incorrect_answers:string[];
+    question:string;
+    type:string
+}
+
+export type QuestionState = Question & { answers : string[]}
+
 export enum Difficulty {
     EASY = 'easy',
     MEDIUM ='medium',
@@ -9,5 +25,10 @@ export const fetchQuizQuestions = async (amount: number, difficulty: Difficulty)
     const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
     // doble await because alot of questions
     const data = await (await fetch(endpoint)).json();
+    return data.results.map((question:Question) => (
+        {...question,
+            answers: shuffleArray([...question.incorrect_answers, question.correct_answer])
+        }
+    ))
     console.log(data)
 }
